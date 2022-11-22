d7619bb6bf8351576b0766f735cf21cc5657916f
Jiachen Sun <sunjiachen1110@gmail.com>
2022-11-21 22:52:18 -0800

    initial setup

--- client/.history/src/App_20221121225043.js
+++ client/.history/src/App_20221121225043.js
@@ -1,3 +1,4 @@
+import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
---
+++ client/src/Components/Menu.js
@@ -0,0 +1,12 @@
+// import "./Menu.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+function Menu() {
+
+    return (
+      <div>Test Menu</div>
+    );
+  }
+  
+  export default Menu;
