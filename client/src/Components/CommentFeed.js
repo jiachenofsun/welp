800fd36eaa523485bcd7d2dd43d991f88a71622c
Raine Koizumi <rainekoizumi@berkeley.edu>
2022-11-29 14:26:56 -0800

    fixed frontend, now front end and back end are connected with basic comments

--- client/src/Components/CommentFeed.js
+++ client/src/Components/CommentFeed.js
@@ -4,24 +4,24 @@ import Comment from "./Comment";
 
 function CommentFeed() {
     const [comments, setComments] = useState();
-    const getcommentsData = () => {
+    const getCommentsData = () => {
       axios
         .get("http://localhost:9000/comments") //THIS IS YOUR URL OF YOUR API
-        .then((data)=>setComments(data.comments)) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
+        .then((data)=>{setComments(data.data.comments); console.log("received comments", data.data.comments)}) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
         .catch((error) => console.log(error));  //ERROR CATCHING IN CASE WE RECIEVE AN ERROR
     };
     
     useEffect(() => {
-      getcommentsData();
+      getCommentsData();
     }, [])
 
     return (
       <div style={{ maxWidth: '600px', marginLeft: 'auto', marginRight: 'auto'}}>
-      {
-        comments && comments.map(d =>
-          <Comment author={d.author} message={d.message} timestamp={d.timestamp} />
-        )
-      }
+        {
+          comments && comments.map(d =>
+            <Comment author={d.author} message={d.message} timestamp={d.timestamp} />
+          )
+        }
 
       </div>
     );
