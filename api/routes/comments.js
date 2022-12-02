79a4c96e7bcaefec784dabe53c88944ee2b28b0b
Raine Koizumi <rainekoizumi@berkeley.edu>
2022-12-01 22:41:24 -0800

    added upvote/downvote to comment backend

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -15,6 +15,34 @@ router.post('/', function(req, res, next) {
   // requires author, message, timestamp, and upvotes, downvotes
     comments = [...comments, {author: req.body.author, messsage:req.body.message, timestamp:req.body.timestamp, upvotes: 0, downvotes: 0}];
     res.json({comments: comments});
-  });
+});
+
+router.post('/upvote/', function(req, res, next) {
+  // should receive dining hall, time, and food item name
+  const author = req.body.author;
+  const timestamp = req.body.timestamp;
+  
+  for (let comment of comments) {
+    if (comment.author === author && comment.timestamp == timestamp) {
+      comment.upvotes++;
+      break;
+    }
+  }
+  res.json({comments: comments});
+});
+
+router.post('/downvote/', function(req, res, next) {
+  // should receive dining hall, time, and food item name
+  const author = req.body.author;
+  const timestamp = req.body.timestamp;
+  
+  for (let comment of comments) {
+    if (comment.author === author && comment.timestamp == timestamp) {
+      comment.upvotes--;
+      break;
+    }
+  }
+  res.json({comments: comments});
+});
 
 module.exports = router;
